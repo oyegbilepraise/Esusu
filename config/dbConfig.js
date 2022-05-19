4a6fbd34fc0b3e36b4a9169288b4d40972ab2fa1
oyegbilepraise <oluwadhammieh@gmail.com>
2022-05-19 12:33:29 +0100

    feat: Updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
 
 module.exports = {
-    // HOST: process.env.DATABASE_HOST,
-    // USER: process.env.DATABASE_USERNAME,
-    // PASSWORD: process.env.DATABASE_PASSWORD,
-    // DB: process.env.DATABASE_NAME,
-    // dialect: 'mysql',
-
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'esusu',
+    HOST: process.env.DATABASE_HOST,
+    USER: process.env.DATABASE_USERNAME,
+    PASSWORD: process.env.DATABASE_PASSWORD,
+    DB: process.env.DATABASE_NAME,
     dialect: 'mysql',
+
+    // HOST: 'localhost',
+    // USER: 'root',
+    // PASSWORD: '',
+    // DB: 'esusu',
+    // dialect: 'mysql',
 
     pool: {
         max: 5,
